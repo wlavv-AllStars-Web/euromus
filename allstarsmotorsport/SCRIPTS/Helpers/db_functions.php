b129b2085a165f2cd20e13bc03472eb6d0d80209
PauloDavidWebSolutions <141250325+PauloDavidWebSolutions@users.noreply.github.com>
2024-01-24 16:48:12 +0000

    db prod

--- allstarsmotorsport/SCRIPTS/Helpers/db_functions.php
+++ allstarsmotorsport/SCRIPTS/Helpers/db_functions.php
@@ -5,7 +5,7 @@ function getConn()
     define('_DB_SERVER_', 'localhost');
     define('_DB_USER_', 'asw200923_user_db');
     define('_DB_PASSWD_', '*U;Z)q%F5T.1');
-    define('_DB_NAME_', 'asw200923_euromuscle_beta');
+    define('_DB_NAME_', 'asw200923_euromuscle');
     return new mysqli("localhost", "asw200923_user_db", "*U;Z)q%F5T.1", "asw200923_euromuscle_beta");
 
     /* check connection */
