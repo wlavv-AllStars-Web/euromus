d2a298f4cc59ac102c3216ce13206abf64cbad7b
PauloDavidWebSolutions <141250325+PauloDavidWebSolutions@users.noreply.github.com>
2024-01-24 16:41:27 +0000

    test 3

--- modules/ukoocompat/views/templates/hook/getValuesForFilter.php
+++ modules/ukoocompat/views/templates/hook/getValuesForFilter.php
@@ -1,7 +1,8 @@
 <?php
 
 include_once $_SERVER['DOCUMENT_ROOT'] . "/allstarsmotorsport/SCRIPTS/Helpers/inc.php";
-
+echo $_SERVER['DOCUMENT_ROOT'];
+exit;
 $sql = "SELECT * FROM eu_ukoocompat_criterion_lang WHERE id_filter=" . ( $_POST['id_filter'] - 1 ) . " AND id_lang = 2";
 
 
